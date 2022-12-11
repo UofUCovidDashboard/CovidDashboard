598293c3a758d8fe22d01106963391dc9242c40e
jmgatto <79178762+jmgatto@users.noreply.github.com>
2022-12-11 13:01:57 -0700

    Updates to FinalGo

--- FinalGo.py
+++ FinalGo.py
@@ -11,9 +11,8 @@
 # Covid Dashboard needs:
 ## Necessary imports
 import json
-from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure
 from bokeh.palettes import GnBu3, OrRd3
-from bokeh.plotting import figure, show
 from datetime import datetime as dt
 from bokeh.layouts import gridplot
 from bokeh.layouts import layout
@@ -24,7 +23,12 @@ from bokeh.layouts import row
 from bokeh.layouts import column
 from bokeh.plotting import figure, output_file, show
 from bokeh.io import curdoc
-from bokeh.plotting import figure, output_file, show
+from bokeh.models import ColumnDataSource, FactorRange
+from bokeh.transform import factor_cmap
+from bokeh.models import ColumnDataSource
+from bokeh.models import LinearAxis, Range1d
+
+from bokeh.models import Range1d
 ## pull the data from the data scrape json files
 days = [2, 3, 5, 6, 7, 8, 9, 10]
 data = []
@@ -84,7 +88,7 @@ for i in range(len(data)):
 dates = [dt(2022, 12, 2), dt(2022, 12, 3), dt(2022, 12, 5), dt(2022, 12, 6), dt(2022, 12, 7), dt(2022, 12, 8), dt(2022, 12, 9), dt(2022, 12, 10)]
 # Plot 1 should contain an overview of all the countries. It will be averaged over all the data. I named it "All Country"
 
-allCountry = figure(x_range = countryNames, height = 250,width = 1500, title="Average Daily Deaths Per Country Over a One Week Period",
+allCountry = figure(x_range = countryNames, height = 200,width = 1500, title="Average Daily Deaths Per Country Over a One Week Period",
                     toolbar_location = None,tools="",x_axis_label='Country', y_axis_label='Daily Deaths')
 allCountry.vbar(x = countryNames,top = temporaryDailyData,width= 0.5, color = "#205072")
 allCountry.xgrid.grid_line_color = None
@@ -94,43 +98,40 @@ allCountry.outline_line_color = "#CFF4D2"
 allCountry.outline_line_width = 3
 
 # Plot 2 should contain a zoomed in view of the selected countries and their details. I named it "Zoomed"
-shortList = ['USA','Taiwan','Mexico','Italy','Germany']
-dates_short = ["12-2","12-3","12-5","12-6","12-7","12-8","12-9","12-10"]
-dummyDailyData = {'death rate': shortList,
-                  '12-2'    : [5, 7, 80, 34, 45],
-                  '12-3'    : [5, 7, 80, 34, 45],
-                  '12-5'    : [5, 7, 80, 34, 45],
-                  '12-6'    : [5, 7, 80, 34, 45],
-                  '12-7'    : [5, 7, 80, 34, 45],
-                  '12-8'    : [5, 7, 80, 34, 45],
-                  '12-9'    : [45, 37, 0, 84, 5],
-                  '12-10'    : [5, 7, 80, 34, 45]
-                                                    }
-dummyNormData = {'death rate': shortList,
-                 '12-2'    : [5, 7, 80, 34, 45],
-                  '12-3'    : [5, 7, 80, 34, 45],
-                  '12-5'    : [5, 7, 80, 34, 45],
-                  '12-6'    : [5, 7, 80, 34, 45],
-                  '12-7'    : [5, 7, 80, 34, 45],
-                  '12-8'    : [5, 7, 80, 34, 45],
-                  '12-9'    : [45, 37, 0, 84, 5],
-                  '12-10'    : [5, 7, 80, 34, 45]
-                                                    }
+shortLists = ['USA','Taiwan','Mexico','Italy','Germany']
+typeDatas = ['Daily','Normalized']
 
-zoomed = figure(y_range = shortList, title="Detailed COVID Death Rate Data for Five Countries",height = 400,width= 500, 
-                x_axis_label='Country', y_axis_label='Daily Deaths')
-zoomed.hbar_stack(dates_short,y = 'Country',width= 0.3, 
-                  source = ColumnDataSource(dummyDailyData))
-zoomed.hbar_stack(dates_short,y = 'Country',width= 0.3, 
-                  source = ColumnDataSource(dummyNormData))
+data = {'Countries'  : shortLists,
+        'Daily'      : [2, 1, 4, 3, 2],
+        'Normalized' : [5, 3, 3, 2, 4]}
+
+palette = ["#c9d9d3", "#718dbf", "#e84d60"]
+
+# this creates [ ("Apples", "2015"), ("Apples", "2016"), ("Apples", "2017"), ("Pears", "2015), ... ]
+x = [ (shortList, typeData) for shortList in shortLists for typeData in typeDatas ]
+counts = sum(zip(data['Daily'], data['Normalized']), ()) # like an hstack
+
+source = ColumnDataSource(data=dict(x=x, counts=counts))
+
+zoomed = figure(x_range=FactorRange(*x), height=300, width = 750, title="Detailed COVID Stats for Select Countries", y_axis_label='Deaths')
+
+zoomed.vbar(x='x', top='counts', width=0.9, source=source, line_color="white",
+       fill_color=factor_cmap('x', palette=palette, factors=typeDatas, start=1, end=2))
+zoomed.yaxis.axis_label = "Daily Death Rates"
+zoomed.y_range = Range1d(-10,250)
+zoomed.extra_y_ranges['foo'] = Range1d(0,1)
+ax2 = LinearAxis(y_range_name = "foo", axis_label = "Normalized Ratio")
+zoomed.add_layout(ax2, 'left')
+zoomed.y_range.start = 0
+zoomed.x_range.range_padding = 0.1
+zoomed.xaxis.major_label_orientation = 1
 zoomed.xgrid.grid_line_color = None
-zoomed.background_fill_color = "#7BE495"
-zoomed.outline_line_color = "#CFF4D2"
-zoomed.outline_line_width = 2
+
+
 
 # Plot 3 should contain an interactive plot with a detailed view. The legend is interactable. I named it "Interactive"
-interactive = figure(title="Daily COVID Death Rates by Country", x_axis_label='Dates', x_axis_type = "datetime", y_axis_label='Daily Deaths',
-                     height = 400, width = 500)
+interactive = figure(title="Daily COVID Death Rates for Select Countries", x_axis_label='Dates', x_axis_type = "datetime", y_axis_label='Daily Deaths',
+                     height = 300, width = 750)
 interactive.line(dates, usaData, legend_label="USA", color="black", line_width=2)
 interactive.line(dates, taiwanData, legend_label="Taiwan", color="#008B8B", line_width=2)
 interactive.line(dates, mexicoData, legend_label="Mexico", color="#FFF8DC", line_width=2)
@@ -138,26 +139,28 @@ interactive.line(dates, italyData, legend_label="Italy", color="#6495ED", line_w
 interactive.line(dates, germanyData, legend_label="Germany", color="#00FA9A", line_width=2)
 #interactive.legend.orientation = "horizontal"
 #interactive.legend.location= "top_center"
+interactive.y_range = Range1d(-10,250)
 interactive.legend.click_policy = "hide"
 interactive.background_fill_color = "#7BE495"
 interactive.outline_line_color = "#CFF4D2"
 interactive.outline_line_width = 2
 
 # Plot 4 should contain historical data over an adjustable period. It needs a slider to adjust the range. I named it "Historical"
-
-historical = figure(title="Historical Daily Death Rate over a Week Long Period", x_axis_label='Dates', x_axis_type = "datetime", y_axis_label='Daily Deaths',
-                    height = 400,width = 500)
-historical.line(dates, usaData, legend_label="USA", color="blue", line_width=2)
+dummyNormData = [0.3, 0.4, 0.5, 0.6, 0.7]
+historical = figure(title="Historical Normalized Death Rate Over a Week Long Period", x_axis_label='Dates', x_axis_type = "datetime", y_axis_label='Normalized Death Rate',
+                    height = 250,width = 1500)
+historical.line(dates, dummyNormData, legend_label="USA", color="blue", line_width=2)
 historical.background_fill_color = "#7BE495"
 historical.outline_line_color = "#CFF4D2"
 historical.outline_line_width = 2
+
 # set up RangeSlider
 date_range_slider = DateRangeSlider(
-    title ="Adjust the data range with the slider",
     value=(dates[2], dates[5]),
     start=dates[0], end=dates[7],
     bar_color ="#56C596",
     step = 1,
+    margin = (5,5,25,5)
 )
 
 date_range_slider.js_link("value", historical.x_range, "start", attr_selector=0)
@@ -167,11 +170,12 @@ date_range_slider.js_link("value", historical.x_range, "end", attr_selector=1)
 # This section contains the details for the dashboard as a whole. 
 overallTitle = Div(
     text="""
-          <p>Covid Dashboard 5250 Fall 2022 </p>
+          <b>Covid Dashboard 5250 Fall 2022 </b>
           """,
     width=750,
     height=10,
-    margin = (5,650,5,650) 
+    margin = (5,650,25,650) 
+    
 )
 spacer = Div(
     text="""
@@ -180,22 +184,51 @@ spacer = Div(
           """,
     width=500,
     height=10,
-    margin = (25,25,25,25) 
+    margin = (25,25,25,25)
+)
+OverallStatistics = Div(
+    text="""
+          <b>  Global Statistics</b>
+          """,
+    width=105,
+    height=15,
+    margin = (5,650,5,700),
+    background = "#CFF4D2",
+)
+SelectStatistics = Div(
+    text="""
+          <b> Statistics for Select Countries</b>
+          """,
+    width=200,
+    height=10,
+    margin = (5,200,15,670) 
 )
 LegendInstructions = Div(
     text="""
-          <p> "Toggle data on and off in the Daily Covid Rates by</p>
-          <p> Country graph by clicking the data on the legend"<p>
+          <i> Toggle data on and off in the "Daily Covid Rates" by Country graph by clicking the data on the legend.</i>
           """,
     width=750,
     height=10,
-    margin = (5,150,5,250) 
+    margin = (5,5,5,850) 
+)
+SliderInstructions = Div(
+    text="""
+          <i>Adjust the data range with the slider:</i>
+          """,
+    width=250,
+    height=10,
+    margin = (5,5,5,500) 
 )
 overall = layout([
     [overallTitle],
+    [OverallStatistics],
     [allCountry],
-    [historical, zoomed,interactive],
-    [spacer, date_range_slider, spacer, LegendInstructions]
+    [historical],
+    [SliderInstructions, date_range_slider],
+    [SelectStatistics],
+    [zoomed,interactive],
+    [LegendInstructions],
+    
 ])
 
 show(overall)
